3cb0e1f18c0e3b8c6307c002c9f3b34f559a8223
justinchan111 <66798499+justinchan111@users.noreply.github.com>
2021-08-01 11:35:48 +0100

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -5,7 +5,7 @@
 #include "processor.h"
 #include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
+// Return the aggregate CPU utilization
 float Processor::Utilization() { 
   std::vector<std::string> cpu_uti = LinuxParser::CpuUtilization();
   return std::stof(cpu_uti[0]);; 
