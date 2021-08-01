f6bd02c922bb333bf84e382aea91760ab35d3983
justinchan111 <66798499+justinchan111@users.noreply.github.com>
2021-08-01 11:18:53 +0100

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -4,10 +4,8 @@
 
 using std::string;
 using std::to_string;
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) {
   int hours, minutes, seconds_remainder;
   string elapsed_time;
